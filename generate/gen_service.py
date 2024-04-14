ef46e384bf6d6fba825b339d6ea8a7eff69a8461
Janus Labs <devops@janustech.com>
2024-04-15 00:18:51 +0100

    Janus DEV

--- generate/gen_service.py
+++ generate/gen_service.py
@@ -10,13 +10,19 @@ config = get_janus_config()
 for process in config["buzzProcesses"]:
     packageName = process["packageName"]
     arr_entity = []
+    hasList = False
     for crud in process["cruds"]:
-        arr_entity.append(cc.make_camel_case(crud["table"]))
+        crud["table"] = cc.make_camel_case(crud["table"])
+        arr_entity.append(crud)
+        for op in crud["ops"]:
+            if op["type"] == "LIST":
+                hasList = True
     content = template.render(
         root_package=config["rootPackage"],
         package=packageName,
         entities=arr_entity,
-        service=cc.make_camel_case(packageName)
+        service=cc.make_camel_case(packageName),
+        hasList=hasList
     )
     root_package = config["rootPackage"].replace(".", "/")
     filename = f"../output/src/main/java/{root_package}/{packageName}/service/{cc.make_camel_case(packageName)}Service.java"
